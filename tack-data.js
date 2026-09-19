5028d49a7f7a83f1b0b9fdfd0410ee95142816f6
simplechives-pixel <simplechives@gmail.com>
2026-09-19 23:19:59 +0100

    Create tack-data.js

---
+++ tack-data.js
@@ -0,0 +1,34 @@
+const tackItems = [
+  {
+    name: "Lavender Bridle",
+    type: "bridle",
+    mod: "tres",
+    images: {
+      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+    }
+  },
+
+  {
+    name: "Rose Saddle",
+    type: "saddle",
+    mod: "bageks",
+    images: {
+      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+    }
+  },
+
+  {
+    name: "Black Blanket",
+    type: "blanket",
+    mod: "tres",
+    images: {
+      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
+      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+    }
+  }
+];
