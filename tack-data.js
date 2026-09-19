c35af91ca332ef32223cc9450b358e73626c28b6
simplechives-pixel <simplechives@gmail.com>
2026-09-20 00:45:54 +0100

    Update tack-data.js

--- tack-data.js
+++ tack-data.js
@@ -1,34 +1,716 @@
 const tackItems = [
   {
-    name: "Lavender Bridle",
-    type: "bridle",
+    name: "Arctic Fly Mask",
+    type: "fly-mask",
     mod: "tres",
     images: {
-      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+      light: "",
+      medium: "",
+      dark: ""
     }
   },
 
   {
-    name: "Rose Saddle",
-    type: "saddle",
+    name: "Black Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Blue Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Brown Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Black Fly Mask",
+    type: "fly-mask",
     mod: "bageks",
     images: {
-      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+      light: "",
+      medium: "",
+      dark: ""
     }
   },
 
   {
-    name: "Black Blanket",
-    type: "blanket",
+    name: "Classic Blood Orange Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Blush Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Burnt Sienna Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Carmine Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Charcoal Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Colbat Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Coral Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Dark Umber Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Electric Blue Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Forest Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Grape Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Honey Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Jade Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Lavender Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Latte Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Merlot Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Midnight Blue Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Mint Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Moss Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Mulberry Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Navy Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Pirate Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Plum Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Rosewood Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Rosey Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Sage Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Sherbert Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Sunlow Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Teal Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Traffic Cone Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Ultramarine Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic White Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Wineberry Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Classic Winter Fly Mask",
+    type: "fly-mask",
+    mod: "bageks",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Creamsicle Fly Mask",
+    type: "fly-mask",
     mod: "tres",
     images: {
-      light: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      medium: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg",
-      dark: "https://file.garden/aNzd9GYmaWI-Oky9/360_F_65473506_rjydmo8sf1stj5KiJhukACvwVHNWxQAf.jpg"
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Cyan Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Dandelion Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Emerald Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Fuchsia Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Gray Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Green Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Lavender Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Light Blue Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Light Gray Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Lime Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Magenta Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Mint Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Navy Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Orange Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Orchid Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Pink Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Purple Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Red Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Rose Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Ruby Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Salmon Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Sunshine Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Topaz Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "White Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
+    }
+  },
+
+  {
+    name: "Yellow Fly Mask",
+    type: "fly-mask",
+    mod: "tres",
+    images: {
+      light: "",
+      medium: "",
+      dark: ""
     }
   }
 ];
