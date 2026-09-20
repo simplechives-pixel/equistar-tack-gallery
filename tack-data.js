5750786ffb861d4e2409033aab1f09879e7e5d1b
simplechives-pixel <simplechives@gmail.com>
2026-09-20 02:33:57 +0100

    Update tack-data.js

--- tack-data.js
+++ tack-data.js
@@ -631,7 +631,7 @@ const tackItems = [
     type: "fly-mask",
     mod: "tres",
     images: {
-      light: "",
+      light: "https://file.garden/aNzd9GYmaWI-Oky9/Tack%20Gallery/Fly%20Masks/Light/Photos_yrE7xxZ3EP.png",
       medium: "",
       dark: ""
     }
